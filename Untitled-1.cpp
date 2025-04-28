ba518fed2a377f131d02fbc96ba38f47b92c76fc
tinwastaken <masacayanjustine@gmail.com>
2025-04-28 17:38:31 +0800

    Add files via upload

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,97 +1,54 @@
 #include <iostream>
 using namespace std;
 
-class Calculator {
+class Employee {
 public:
-    int add(int a, int b) {
-        return a + b;
+    virtual void showDetails() {
+        cout << "Employee details" << endl;
     }
+    virtual ~Employee() {} 
+};
 
-    int add(int a, int b, int c) {
-        return a + b + c;
+class Manager : public Employee {
+public:
+    void showDetails() override {
+        cout << "I am a Manager. I manage teams." << endl;
     }
+};
 
-    float add(float a, float b) {
-        return a + b;
-    }
-
-    int subtract(int a, int b) {
-        return a - b;
-    }
-
-    int multiply(int a, int b) {
-        return a * b;
-    }
-
-    float divide(float a, float b) {
-        if (b == 0) {
-            cout << "Error: Division by zero!" << endl;
-            return 0;
-        }
-        return a / b;
+class Developer : public Employee {
+public:
+    void showDetails() override {
+        cout << "I am a Developer. I write code." << endl;
     }
 };
 
 int main() {
-    Calculator calc;
+    cout << "What type of employee would you like to create?" << endl;
+    cout << "1. Manager" << endl;
+    cout << "2. Developer" << endl;
+    cout << "\nEnter your choice (1 or 2): ";
+    
     int choice;
-
-    cout << "=== Simple Calculator ===" << endl;
-    cout << "1. Add two integers" << endl;
-    cout << "2. Add three integers" << endl;
-    cout << "3. Add two floats" << endl;
-    cout << "4. Subtract two integers" << endl;
-    cout << "5. Multiply two integers" << endl;
-    cout << "6. Divide two floats" << endl;
-    cout << "Enter your choice (1-6): ";
     cin >> choice;
-
-    switch (choice) {
-        case 1: {
-            int a, b;
-            cout << "Enter two integers: ";
-            cin >> a >> b;
-            cout << "Result: " << calc.add(a, b) << endl;
+    
+    Employee* emp = nullptr;  
+    
+    switch(choice) {
+        case 1:
+            emp = new Manager();
             break;
-        }
-        case 2: {
-            int a, b, c;
-            cout << "Enter three integers: ";
-            cin >> a >> b >> c;
-            cout << "Result: " << calc.add(a, b, c) << endl;
+        case 2:
+            emp = new Developer();
             break;
-        }
-        case 3: {
-            float a, b;
-            cout << "Enter two floats: ";
-            cin >> a >> b;
-            cout << "Result: " << calc.add(a, b) << endl;
-            break;
-        }
-        case 4: {
-            int a, b;
-            cout << "Enter two integers: ";
-            cin >> a >> b;
-            cout << "Result: " << calc.subtract(a, b) << endl;
-            break;
-        }
-        case 5: {
-            int a, b;
-            cout << "Enter two integers: ";
-            cin >> a >> b;
-            cout << "Result: " << calc.multiply(a, b) << endl;
-            break;
-        }
-        case 6: {
-            float a, b;
-            cout << "Enter two floats: ";
-            cin >> a >> b;
-            cout << "Result: " << calc.divide(a, b) << endl;
-            break;
-        }
         default:
             cout << "Invalid choice!" << endl;
+            return 1;
     }
-
+    
+    emp->showDetails();  
+    
+    delete emp;  
+    
     return 0;
 }
